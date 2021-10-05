cfe7c99ee2743aaa4be87ecfb62d17f75c603fa6
Doni-zete <donizete112@gmail.com>
2021-10-04 23:21:44 -0300

    adicionando arquivos

---
+++ personapi/src/test/java/one/ditalinnovation/personapi/PersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package one.ditalinnovation.personapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
