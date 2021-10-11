e6aeef5ae9aa1b56d75bce04705e9b73bff0f427
Doni-zete <donizete112@gmail.com>
2021-10-10 23:03:59 -0300

    atualizando

--- src/main/java/one/ditalinnovation/personapi/entity/Person.java
+++ src/main/java/one/ditalinnovation/personapi/entity/Person.java
@@ -1,20 +1,13 @@
 package one.ditalinnovation.personapi.entity;
 
 
-import java.time.LocalDate;
-import java.util.List;
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
 
 
 @Data
@@ -41,8 +34,7 @@ public class Person {
 
     private LocalDate birthDate;
 
-    @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
-    private List<Phone> phones;
+
 
 }
-}
+
